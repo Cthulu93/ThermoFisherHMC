e31c5edbf45b2a27c72e3cbc949c32a060be55a6
root <root@ip-172-31-24-93.ec2.internal>
2014-04-20 18:59:28 -0400

    HOLY SHIT

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,33 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import boto.dynamodb2
+from boto.dynamodb2.fields import HashKey, RangeKey, KeysOnlyIndex, AllIndex
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.types import NUMBER
+import decimal
+import time
+
+# creates plots from the last 10 minutes
+current_time = time.time()
+
+data = Table('raspi')
+result = data.query_2(station__eq='home',date__gte=current_time-600)
+
+
+temp_vector = []
+light_vector = []
+hum_vector = []
+time_vector = []
+
+for res in result:
+	temp_vector.append(res['temperature'])
+	light_vector.append(res['light'])
+	hum_vector.append(res['humidity'])
+
+time_vector = [i*(600/len(temp_vector)) for i in range(len(temp_vector))]	
+
 
 def calcHeatIndex(temp, hum):
     tempinF = float(C) * 9 / 5 + 32
@@ -16,18 +44,23 @@ def calcHeatIndex(temp, hum):
                 c6*(hum**2) + c7*(temp**2)*hum + c8*temp*(hum**2) + c9*(temp**2)*(hum**2)
     return heatIndex
 
-def simpleDataPlot(time, data, color='b'):
+def simpleDataPlot(time, data, color='b', filename='lastFig.png'):
     timeArray = np.array(time)
     datArray = np.array(data)
     plt.plot(timeArray, datArray, color)
     #plt.show()
-    plt.savefig('lastFig.png', dpi=300)
+    plt.savefig(filename, dpi=300)
 
-def plotTempHumLight(time, temp, hum, light):
+def plotTempHumLight(time, temp, hum, light, filename = 'lastFig.png'):
     timeArray = np.array(time)
     tempArray = np.array(temp)
     humArray = np.array(hum)
     lightArray = np.array(light)
-    plt.plot(timeArray, tempArray, 'r', timeArray, humArray, 'g',
+    p1 = plt.plot(timeArray, tempArray, 'r', timeArray, humArray, 'g',
              timeArray, lightArray, 'b')
-    plt.savefig('lastFig.png', dpi=300)
+    plt.legend(p1,['Temperature','Humidity','Light'],loc=2)
+    plt.savefig(filename, dpi=300)
+    
+
+
+plotTempHumLight(time_vector, temp_vector, hum_vector, light_vector)
